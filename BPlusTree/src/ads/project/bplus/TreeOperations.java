5109d428ae57b16114f6aaf35cb73911ac0ec837
karantix <karanchauhan2@gmail.com>
2017-11-07 21:15:56 -0500

    test

--- BPlusTree/src/ads/project/bplus/TreeOperations.java
+++ BPlusTree/src/ads/project/bplus/TreeOperations.java
@@ -1,11 +1,24 @@
 package ads.project.bplus;
 
+import java.util.List;
+
 public class TreeOperations {
 
 	public void Initialize(int m) {
 		
 	}
 	
+	public void Insert(double key, String value) {
+		
+	}
+	
+	public String Search(double key) {
+		return null;
+	}
+	
+	public List<String> Search(double key1, double key2) {
+		return null;
+	}
 	
 	
 }
